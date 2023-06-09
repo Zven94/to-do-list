feca437f787d7b4f8e6b8f4a7af09555cfc2660f
Nicolas Bolañez <nicobolanez@gmail.com>
2023-06-09 08:51:19 -0300

    added function to edit dinamicaly the task

--- src/add-remove.js
+++ src/add-remove.js
@@ -1,3 +1,6 @@
+import editTask from './editTask.js';
+
+const reditTask = editTask;
 
 export default class Task {
   static initialize() {
@@ -27,28 +30,42 @@ export default class Task {
       
       const elementLi = document.createElement('li');
       const elementDiv = document.createElement('div');
+      const elementDiv2 = document.createElement('div');
       const elementInput = document.createElement('input');
-      const elementLabel = document.createElement('label');
+      const elementLabel = document.createElement('input');
       const elementButton = document.createElement('button');
       const elementDeleteButton = document.createElement('button');
       elementButton.classList.add('threeDots');
+      elementDeleteButton.classList.add('elementDeleteButton');
+      elementDiv.classList.add('elementDiv1');
+      elementDiv2.classList.add('elementDiv2');
+      elementLabel.classList.add('taskLabel')
     
       this.divLi.appendChild(elementLi);
       elementLi.appendChild(elementDiv);
       elementDiv.appendChild(elementInput);
       elementDiv.appendChild(elementLabel);
-      elementLi.appendChild(elementDeleteButton);
-      elementLi.appendChild(elementButton);
+      elementLi.appendChild(elementDiv2)
+      elementDiv2.appendChild(elementDeleteButton);
+      elementDiv2.appendChild(elementButton);
       elementInput.type = 'checkbox';
-      elementLabel.textContent = `${task.taskToDo}`;
+      elementLabel.value = `${task.taskToDo}`;
       elementDeleteButton.textContent = 'X';
       elementButton.textContent = '...';
 
       elementDeleteButton.addEventListener('click', () => {
         this.removeTask(index);
       });
-      
-      //this.taskList.insertBefore(elementLi, this.taskList.firstChild);
+
+      elementLabel.addEventListener('focusin', () => {
+        elementLi.style.boxShadow = '0 0 10px rgba(5, 110, 200, 0.8)';
+      });
+      elementLabel.addEventListener('focusout', () => {
+        elementLi.style.border = '1px solid rgba(0, 0, 0, 0.15)';
+        elementLi.style.boxShadow = 'none';
+        console.log(elementLabel.value)
+        this.saveTask;
+      });
     });
 
   }
